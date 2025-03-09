5d6c40376bcecbe7b00b58807ee23eafe6956ccf
100542 <152491862+100542@users.noreply.github.com>
2025-03-09 20:50:27 +0100

    cleanup

--- resources/views/market.blade.php
+++ resources/views/market.blade.php
@@ -36,36 +36,7 @@
     </div>
 </div>
 
-<script>
-    async function fetchCryptoData() {
-        try {
-            const response = await fetch("{{ route('crypto.data') }}");
-            const data = await response.json();
-
-            let tableBody = document.getElementById("crypto-table-body");
-            tableBody.innerHTML = "";
-
-            Object.values(data).forEach(coin => {
-                let row = `
-                        <tr class="border border-white/30">
-                            <td class="text-yellow-300 border border-white/30 px-4 py-2">${coin.symbol}</td>
-                            <td class="border border-white/30 px-4 py-2">${coin.open}</td>
-                            <td class="border border-white/30 px-4 py-2">${coin.high}</td>
-                            <td class="border border-white/30 px-4 py-2">${coin.low}</td>
-                            <td class="border border-white/30 px-4 py-2">${coin.close}</td>
-                            <td class="border border-white/30 px-4 py-2">${coin.volume}</td>
-                        </tr>
-                    `;
-                tableBody.innerHTML += row;
-            });
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
-    }
-
-    fetchCryptoData();
-    setInterval(fetchCryptoData, 1000);
-</script>
+<script src="{{ asset('js/market.js') }}" defer></script>
 
 </body>
 </html>
