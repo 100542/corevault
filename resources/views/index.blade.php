37b1b3f8a505298335585e9c312d451ced787440
100542 <152491862+100542@users.noreply.github.com>
2025-04-07 21:13:58 +0200

    nieuwe front-end fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,31 +11,18 @@
     @endif
 </head>
 <body>
-<div class="flex flex-col justify-center min-h-screen bg-gray-800 text-white">
-    <div class="relative z-10 flex flex-col justify-center items-center space-y-8 text-center">
+    @include('nav')
 
-        <h1 class="text-9xl font-bold">Corevault</h1>
-        <h2 class="text-3xl font-light tracking-widest text-white/90">
-            Crypto. Organised. Simplified.
-        </h2>
-
-        <p class="text-lg max-w-2xl text-white/80">
-            Securely store, manage, and trade your digital assets with ease.
-            Join thousands of users protecting their crypto investments with Corevault.
-        </p>
-        <div class="flex space-x-4">
-            <a href="{{ route('login') }}" class="px-6 py-3 w-52 bg-[#C9C7BA] text-[#29292B] rounded-md text-lg font-semibold hover:bg-[#29292B] hover:text-[#C9C7BA] duration-300">
-                Log In
-            </a>
-            <a href="{{ route('register.page') }}" class="px-6 w-52 py-3 border border-[#C9C7BA] text-[#C9C7BA] rounded-md text-lg font-semibold hover:bg-[#C9C7BA] hover:text-[#29292B] duration-300">
-                Get Started
-            </a>
-        </div>
-        <div class="mt-12 max-w-lg text-white/80 text-sm">
-            <p>This is a fake application to practise the Laravel framework.</p>
-            <p>Do not enter real information.</p>
-        </div>
-    </div>
-</div>
+    <main>
+        <section>
+            <div class="flex flex-row w-screen h-[100dvh] justify-center items-center">
+                <div class="flex flex-col justify-center items-center">
+                    <h1 class="text-[#C4FFCE] text-center text-8xl font-black">Crypto, Simplified</h1>
+                    <h1 class="text-[#8FB996] text-5xl font-black text-center mt-4">Corevault</h1>
+                    <a href={{ route('login') }}><button type="submit" class="mt-8 border-2 border-[#C4FFCE] p-4 rounded-md shadow-md min-w-32 text-xl font-black text-[#C4FFCE]">Get Started</button></a>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -10,36 +10,38 @@
         <script src="https://cdn.tailwindcss.com"></script>
     @endif
 </head>
-<body class="bg-gray-900 text-white">
+<body class="bg-[#040604] text-white">
 
-<nav class="sticky top-0 w-full h-20 bg-gray-800 shadow-md flex justify-between items-center px-8">
+<nav class="backdrop-blur-2xl absolute top-0 w-full h-20 shadow-md flex justify-between items-center px-8">
     <div class="flex flex-row gap-4">
-        <h1 class="font-bold text-3xl">Corevault</h1>
+        <img src="/logo.svg" alt="Logo" class="w-14 h-14 hover:rotate-[360deg] duration-300">
     </div>
 
     <div class="flex gap-16 tracking-wide text-lg">
-        <a href="{{ route('dashboard') }}" class="hover:text-gray-400 transition {{ request()->routeIs('dashboard') ?
-'text-yellow-300' : '' }}">Dashboard</a>
-        <a href="{{ route('market.page') }}" class="hover:text-gray-400 transition {{ request()->routeIs('market.page') ?
-'text-yellow-300' : '' }}">Markets</a>
+        <a href="{{ route('dashboard') }}" class="hover:scale-105 border-2 border-[#C4FFCE] text-[#C4FFCE] p-3 font-black rounded-lg shadow-md transition {{ request()->routeIs('dashboard') ?
+'text-yellow-300 border-yellow-300' : '' }}">Dashboard</a>
+        <a href="{{ route('market.page') }}" class="hover:scale-105 border-2 border-[#C4FFCE] text-[#C4FFCE] p-3 font-black rounded-lg shadow-md transition {{ request()->routeIs('market.page') ?
+'text-yellow-300 border-yellow-300' : '' }}">Markets</a>
         <a href="{{ route('dashboard') }} {{ request()->routeIs('dashboard') ?
-'text-yellow-300' : '' }}" class="hover:text-gray-400 transition">Trade</a>
+'text-yellow-300 border-yellow-300' : '' }}" class="hover:scale-105 border-2 border-[#C4FFCE] text-[#C4FFCE] p-3 font-black rounded-lg shadow-md transition ">Trade</a>
         <a href="{{ route('wallets.page') }} {{ request()->routeIs('wallets.page') ?
-'text-yellow-300' : '' }}" class="hover:text-gray-400 transition">Wallets</a>
+'text-yellow-300 border-yellow-300' : '' }}" class="hover:scale-105 border-2 border-[#C4FFCE] text-[#C4FFCE] p-3 font-black rounded-lg shadow-md transition ">Wallets</a>
         <a href="{{ route('dashboard') }} {{ request()->routeIs('dashboard') ?
-'text-yellow-300' : '' }}" class="hover:text-gray-400 transition">History</a>
+'text-yellow-300 border-yellow-300' : '' }}" class="hover:scale-105 border-2 border-[#C4FFCE] text-[#C4FFCE] p-3 font-black rounded-lg shadow-md transition ">History</a>
     </div>
 
     <div class="relative group">
         <button class="focus:outline-none flex items-center space-x-2">
-            <span class="text-2xl">{{ Auth::user()->username }}</span>
-            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <span class="text-[#C4FFCE] text-4xl p-3 font-black rounded-lg shadow-md transition ">{{ Auth::user()->username }}</span>
+            <svg class="w-5 h-5" fill="none" stroke="#C4FFCE" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
             </svg>
         </button>
 
-        <div class="absolute right-0 mt-2 w-40 bg-gray-700 rounded-md shadow-lg hidden group-hover:block">
-            <a href="{{ route('logout') }}" class="text-3xl px-4 py-2 hover:bg-gray-600">Logout</a>
+        <div class="absolute right-0 rounded-md p-4 min-w-96 border-[#C4FFCE] border-2 shadow-lg hidden group-hover:flex flex-col ">
+            <p class="text-2xl font-black text-[#C4FFCE]">Hello, <span class="text-2xl text-[#C4FFCE]">{{ Auth::user() ->username }}.</span></p>
+            <a href="{{ route('dashboard') }}" class="mt-4 text-[#C4FFCE]">Manage profile</a>
+            <a href="{{ route('logout') }}" class="text-[#C4FFCE]">Logout</a>
         </div>
     </div>
 </nav>
